a10eb5bbb5515179c707c71723d206469b85ad17
Manuel Gutierrez Casares <91099270+ManuelGutierrezCasares@users.noreply.github.com>
2025-07-12 16:09:27 -0300

    Implementation of frontend

---
+++ script.js
@@ -0,0 +1,80 @@
+let signerAddress;
+let SimpleSwap;
+let tokenA;
+let tokenB;
+let connected;
+let amountTokenA;
+let amountTokenB;
+let signer;
+
+
+
+init();
+
+async function init() {
+    const provider = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/f300225e203c45b9ba70c0daef9cc172");
+    SimpleSwap = new ethers.Contract(contractAddress, contractAbi, provider);
+    tokenA = new ethers.Contract(tokenAAddress, tokenAAbi, provider);
+    tokenB = new ethers.Contract(tokenBAddress, tokenBAbi, provider);
+    connected = false;
+}
+
+
+async function connect()
+{
+    if (typeof window.ethereum !== undefined) {
+        await window.ethereum.request({"method": "eth_requestAccounts","params": [],});
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        signerAddress = await signer.getAddress();
+        document.getElementById("accountAddress").innerText = signerAddress;
+        connected = true;
+        updateLiquidity();
+    } else {
+        alert("creese una metamask");
+    }
+}
+
+async function handleSubmit() {
+    if (!connected) {
+        alert("creese una metamask");
+    } else {
+        const AmountToBuy = document.querySelector("#form > input.IHAVE").value;
+        let tokenABalanceOfSigner, tokenBBalanceOfSigner;
+        try {
+            tokenABalanceOfSigner = await tokenA.balanceOf(signerAddress);
+            tokenBBalanceOfSigner = await tokenB.balanceOf(signerAddress);
+        } catch (error) {
+            tokenABalanceOfSigner = null;
+            tokenBBalanceOfSigner = null;
+        }
+
+        if (!tokenABalanceOfSigner && !tokenBBalanceOfSigner) {
+            alert('Usted no tenía balance de los tokens MAN1 o MAN2, se le han minteado para el uso de esta DAPP');
+            tokenA.approve(signerAddress);
+            tokenA.mint(100000000);
+            tokenB.approve(signerAddress);
+            tokenB.mint(100000000);
+        }
+        
+        if (isNaN(amountTokenA) || isNaN(amountTokenB)) {
+            alert('You need to use numbers.')
+        } else {
+            let contractSigned = new ethers.Contract(contractAddress, contractAbi, signer);
+            const tx = await contractSigned.swapExactTokensForTokens(Number(amountTokenA), Number(amountTokenB), [tokenAAddress, tokenBAddress]);
+            await tx.wait();
+            updateLiquidity();
+            alert('Your transaction has been completed. Congratulations.')
+        }
+    }
+}
+
+async function updateLiquidity() {
+    const price = await SimpleSwap.getPrice(tokenAAddress, tokenBAddress);
+    document.getElementById('liquidityPriceDisplay').innerText = Number(price);
+}
+
+function setValueTokenToSpend() {
+	amountTokenA = document.getElementsByClassName("IHAVE")[0].value;
+	amountTokenB = document.getElementsByClassName("IWANT")[0].value;
+}
